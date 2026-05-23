00adf5632217927581e4e0fa80880df3f71542c5
Jayanta Biswas <jayanta.biswas@delostylestudio.com>
2026-05-23 23:04:52 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>This is just one file created by jayanta biswas</h1>
+          <h1>Jay Biswas</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
