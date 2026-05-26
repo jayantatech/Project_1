a0c5514409bc2dec134cb3866a0a26d08b35b2f5
Jayanta Biswas <jayanta.biswas@delostylestudio.com>
2026-05-27 01:00:59 +0530

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Jay Biswas</h1>
+          <h1>It is working fine and dandy!</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
